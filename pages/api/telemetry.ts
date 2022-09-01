84cbfa20e3bd60ec7792b8aedee8bc67ca98f0d3
Felix <felix@tellmann.co.za>
2022-09-01 21:50:41 +0200

    Refactored Icon components

--- pages/api/telemetry.ts
+++ pages/api/telemetry.ts
@@ -11,10 +11,15 @@ type TelemetryFunction = (
 
 export const Telemetry: TelemetryFunction = async (req, res) => {
   console.log(req.body);
+  // console.log(req.headers);
   const data = await fetch("https://flext-analytics.vercel.app/api/collect", {
+    headers: {
+      "User-Agent": req.headers["user-agent"] as string,
+    },
     method: "POST",
     body: req.body,
   });
+
   console.log({ data });
   res.status(200).json({ name: "John Doe" });
 };
