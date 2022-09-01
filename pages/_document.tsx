f4c802eaa1b1139ae0630b86e21549bb19c67b35
Felix <felix@tellmann.co.za>
2022-09-01 21:15:33 +0200

    Refactored Icon components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@
 import { Favicon } from "_client/_document/favicon";
 import { Font } from "_client/_document/font";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class Root extends Document {
   render(): JSX.Element {
@@ -11,6 +12,14 @@ class Root extends Document {
         <Head>
           <Favicon />
           <Font />
+
+          <Script
+            async
+            defer
+            src="https://flext-analytics.vercel.app/umami.js"
+            strategy="lazyOnload"
+            data-website-id="69aaf2ad-a456-4a0e-9d99-b31687decc50"
+          />
         </Head>
         <body className="color-gray--slate bg-white [--line-color:theme(colors.gray.200/0.8)] d:bg-gray-900 d:bg-gradient-to-b d:from-black/40 d:to-black/40">
           <Main />
