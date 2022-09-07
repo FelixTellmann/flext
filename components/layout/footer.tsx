1d5429f2dbf620e949782114c513a5d8f20d1ea2
Felix <felix@tellmann.co.za>
2022-09-07 18:57:53 +0200

    CV Darkmode upgrade

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,18 +6,22 @@ type FooterProps = {};
 
 export const Footer: FC<FooterProps> = ({}) => {
   return (
-    <footer className="bg-white">
+    <footer>
       <div className="mx-auto max-w-6xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {Object.values(SOCIAL_ACCOUNTS).map(({ name, Icon, href }) => (
-            <Link key={name} href={href} className="p-1 text-gray-400 hover:text-gray-500">
+            <Link
+              key={name}
+              href={href}
+              className="p-1 text-gray-400 hover:text-gray-500 d:text-gray-600 d:hover:text-gray-500"
+            >
               <span className="sr-only">{name}</span>
               <Icon className="h-5 w-5" aria-hidden="true" />
             </Link>
           ))}
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
-          <small className="block text-center text-sm text-gray-400">
+          <small className="block text-center text-sm text-gray-400 d:text-gray-600">
             &copy; {new Date().getFullYear()} Felix Tellmann, All rights reserved.
           </small>
         </div>
