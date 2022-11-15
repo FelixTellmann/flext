4be78d2fedb158f8ba9cb5e94eec569423577dce
Felix Tellmann <felix@tellmann.co.za>
2022-11-15 13:38:34 +0200

    Liz Resume WIP

--- pages/api/typefrom-webhook.ts
+++ pages/api/typefrom-webhook.ts
@@ -11,6 +11,7 @@ type TypeformWebhookFunction = (
 
 export const TypeformWebhook: TypeformWebhookFunction = async (req, res) => {
   console.log(req.body);
+  console.log(JSON.stringify(req.body));
   res.status(200).json({ name: "John Doe" });
 };
 
